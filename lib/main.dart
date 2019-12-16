706b456e00a82b329e680036cc166959575e293a
mmtolsma <mtolsma19@gmail.com>
2019-12-16 12:10:04 +0200

    Josh fixed issue with first page not loading

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,8 @@ import 'package:http/http.dart' as http;
 void main() => runApp(MaterialApp(
       title: "Local Hour application",
       debugShowCheckedModeBanner: false,
-      home: MyTabs(), //Change to HomePage when wanting first clicking page
-    )
-);
+      home: MyTabs(),
+    ));
 
 class MyTabs extends StatefulWidget {
   @override
@@ -17,22 +16,30 @@ class MyTabs extends StatefulWidget {
 }
 
 class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin {
-
   final String url = 'https://local.ponelat.com'; //API url
   List specials;
   TabController controller;
+
+  List<Widget> myTabs = [
+    Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Text("Today", style: TextStyle(fontSize: 15.0)),
+    ),
+    Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Text("Tomorrow", style: TextStyle(fontSize: 15.0)),
+    ),
+    Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Text("Next Day", style: TextStyle(fontSize: 15.0)),
+    ),
+  ];
 
   @override
   void initState() {
     super.initState();
 
     controller = TabController(vsync: this, length: 3);
-
-    this.getSpecials().then((jsonSpecials) {
-      setState(() {
-        specials = jsonSpecials;
-      });
-    });
   }
 
   @override
@@ -42,45 +49,41 @@ class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin {
   }
 
   Future<List> getSpecials() async {
-    var response = await http.get(
-        Uri.encodeFull(url + '/specials'),
+    var response = await http.get(Uri.encodeFull(url + '/specials'),
         headers: {"Accept": "application/json"});
     return jsonDecode(response.body);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.dehaze), //there's an "action" option for menus and stuff. "leading" for show
-        title: specials == null ? Text("LOCAL HOUR") : Text("Now with more special"),
-        backgroundColor: Colors.green,
-        bottom: TabBar(
-          controller: controller,
-          tabs: [
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Text("Today", style: TextStyle(fontSize: 15.0)),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Text("Tomorrow", style: TextStyle(fontSize: 15.0)),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Text("Next Day", style: TextStyle(fontSize: 15.0)),
-            ),
-          ],
-        )
-      ),
-      body: TabBarView(
-        controller: controller,
-        children: <Widget>[
-          SecondPage(specials: specials),
-          SecondPage(specials: specials),
-          SecondPage(specials: specials),
-        ],
-      ),
+    print("Build");
+    return FutureBuilder(
+      future: getSpecials(),
+      builder: (context, snapshot) {
+        print("The data is busy loading");
+        if (!snapshot.hasData) {
+          return Center(child: CircularProgressIndicator());
+        }
+        return Scaffold(
+          appBar: AppBar(
+              leading: Icon(Icons.dehaze),
+              //there's an "action" option for menus and stuff. "leading" for show
+              title: Text("LOCAL HOUR"),
+              backgroundColor: Colors.green,
+              bottom: TabBar(
+                controller: controller,
+                tabs: myTabs.toList(),
+              )),
+          body: TabBarView(
+            controller: controller,
+            children: <Widget>[
+              SecondPage(specials: snapshot.data),
+              SecondPage(specials: snapshot.data),
+              SecondPage(specials: snapshot.data),
+            ],
+          ),
+        );
+      },
     );
   }
 }
