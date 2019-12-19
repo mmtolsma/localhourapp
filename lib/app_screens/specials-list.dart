1f91373ae92fbbd405758d9b97a894a6fd3cfd02
mmtolsma <mtolsma19@gmail.com>
2019-12-19 15:56:09 +0200

    Josh fixed the 'pull-to-refresh'; built apk for this version

--- lib/app_screens/specials-list.dart
+++ lib/app_screens/specials-list.dart
@@ -3,17 +3,22 @@ import 'package:swapi_project/components/specials-list-item.dart';
 
 class SpecialsList extends StatefulWidget {
   final List specials;
+  final Function refresh;
+  final int specialsIndex;
 
-  SpecialsList({this.specials});
+  SpecialsList({this.specials, this.refresh, this.specialsIndex});
 
   @override
-  SpecialsListState createState() => SpecialsListState(specials: specials);
+  SpecialsListState createState() => SpecialsListState(
+      specials: specials, refresh: refresh, specialsIndex: specialsIndex);
 }
 
 class SpecialsListState extends State<SpecialsList> {
   List specials;
+  Function refresh;
+  int specialsIndex;
 
-  SpecialsListState({this.specials});
+  SpecialsListState({this.specials, this.refresh, this.specialsIndex});
 
   @override
   Widget build(BuildContext context) {
@@ -42,40 +47,50 @@ class SpecialsListState extends State<SpecialsList> {
   }
 
   Future<Null> refreshList() async {
-    await Future.delayed(Duration(seconds: 2));
-
-    setState(() {
-      listOfSpecies();
-//      print("Done!");
+    Future f = refresh(specialsIndex);
+    f.then((newSpecials) {
+      setState(() {
+        specials = newSpecials;
+      });
     });
-
+    await f;
     return null;
   }
 
   Widget listOfSpecies() {
     return specials.isEmpty
-        ? Center(
-            child: Text("No specials today!",
-            style: TextStyle(fontSize: 20.0),
-          ))
-        : RefreshIndicator(
-                onRefresh: refreshList,
-                child: ListView.builder(
-                  itemCount: specials == null ? 0 : specials.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return speciesListItem(index);
-                  },
+        ? RefreshIndicator(
+            onRefresh: refreshList,
+            child: Center(
+              child: SingleChildScrollView(
+                physics: AlwaysScrollableScrollPhysics(),
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 400.0, //BAD! Need a way to fix this height issue!
+                  width: MediaQuery.of(context).size.width,
+                  child: Text('No specials here!', style: TextStyle(fontSize: 30.0),),
                 ),
-        );
+              ),
+            ),
+          )
+        : RefreshIndicator(
+            child: ListView.builder(
+              itemCount: specials == null ? 0 : specials.length,
+              itemBuilder: (BuildContext context, int index) {
+                return speciesListItem(index);
+              },
+            ),
+            onRefresh: refreshList,
+          );
   }
 
   Widget speciesListItem(int index) {
     return SpecialsListItem(
-        venue: specials[index]['venue'],
-        dayOfWeek: specials[index]['dayOfWeek'],
-        specialTime: specials[index]['specialTime'],
-        specialSummary: specials[index]['specialSummary'],
-        specialDetails: specials[index]['specialDetails'],
+      venue: specials[index]['venue'],
+      dayOfWeek: specials[index]['dayOfWeek'],
+      specialTime: specials[index]['specialTime'],
+      specialSummary: specials[index]['specialSummary'],
+      specialDetails: specials[index]['specialDetails'],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:swapi_project/components/OptionsMenu.dart';
 import 'app_screens/specials-list.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:swipedetector/swipedetector.dart';
 
 void main() => runApp(MaterialApp(
       title: "Local Hour application",
@@ -19,22 +18,23 @@ class MyTabs extends StatefulWidget {
 
 class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin {
   final String url = 'https://local.ponelat.com'; //API url
-  List specials;
   TabController controller;
   int tabIndex = 0;
+  Future<List> specials;
+  int counter = 0;
 
   List<Widget> myTabs(List views) {
     return [
         Padding(
-          padding: const EdgeInsets.all(15.0),
+          padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
           child: Text(views[0]['title'], style: TextStyle(fontSize: 15.0)),
         ),
         Padding(
-          padding: const EdgeInsets.all(15.0),
+          padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
           child: Text(views[1]['title'], style: TextStyle(fontSize: 15.0)),
         ),
         Padding(
-          padding: const EdgeInsets.all(15.0),
+          padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
           child: Text(views[2]['title'], style: TextStyle(fontSize: 15.0)),
         ),
       ];
@@ -43,9 +43,8 @@ class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-
+    specials = getSpecials();
     controller = TabController(vsync: this, length: 3);
-
     controller.addListener(updateTabColorFromControllerIndex);
   }
 
@@ -65,13 +64,22 @@ class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin {
   Future<List> getSpecials() async {
     var response = await http.get(Uri.encodeFull(url + '/specials-views'),
         headers: {"Accept": "application/json"});
-    return jsonDecode(response.body);
+    var jsonData = jsonDecode(response.body);
+    return jsonData;
+  }
+
+  Future<List> getSpecialsForIndex(int index) {
+    var f = getSpecials();
+    setState(() {
+      specials = f;
+    });
+    return f.then((data) => data[index]["specials"]);
   }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: getSpecials(),
+      future: specials,
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
           return Column(
@@ -95,8 +103,20 @@ class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin {
         }
         return Scaffold(
           appBar: AppBar(
-              leading: Icon(Icons.dehaze),
-              title: Text("LOCAL HOUR"),
+              title: Text("LOCALHOUR"),
+              actions: <Widget>[
+                PopupMenuButton<String>(
+                  onSelected: choiceAction,
+                  itemBuilder: (BuildContext context) {
+                    return MenuItems.choices.map((String choice) {
+                      return PopupMenuItem<String> (
+                        value: choice,
+                        child: Text(choice),
+                      );
+                    }).toList();
+                  },
+                )
+              ],
               bottom: TabBar(
                   controller: controller,
                   tabs: myTabs(snapshot.data),
@@ -109,33 +129,57 @@ class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin {
               )
           ),
           body: TabBarView(
-            dragStartBehavior: DragStartBehavior.start,
             controller: controller,
             children: <Widget>[
               Scaffold(
                   appBar: AppBar(
-                      backgroundColor: colorForTab(tabIndex),
-                      centerTitle: true,
-                      title: Text(snapshot.data[0]['heading'])),
-                  body: SpecialsList(specials: snapshot.data[0]['specials'])),
+                    backgroundColor: colorForTab(tabIndex),
+                    centerTitle: true,
+                    title: Text(snapshot.data[0]['heading'])),
+                  body: SpecialsList(
+                    specials: snapshot.data[0]['specials'],
+                    specialsIndex: 0,
+                    refresh: getSpecialsForIndex,
+                  )),
               Scaffold(
                   appBar: AppBar(
-                      backgroundColor: colorForTab(tabIndex),
-                      centerTitle: true,
-                      title: Text(snapshot.data[1]['heading'])),
-                  body: SpecialsList(specials: snapshot.data[1]['specials'])),
+                    backgroundColor: colorForTab(tabIndex),
+                    centerTitle: true,
+                    title: Text(snapshot.data[1]['heading'])),
+                  body: SpecialsList(
+                    specials: snapshot.data[1]['specials'],
+                    specialsIndex: 1,
+                    refresh: getSpecialsForIndex,
+                  ),
+
+              ),
               Scaffold(
                   appBar: AppBar(
                       backgroundColor: colorForTab(tabIndex),
                       centerTitle: true,
                       title: Text(snapshot.data[2]['heading'])),
-                  body: SpecialsList(specials: snapshot.data[2]['specials'])),
+                  body: SpecialsList(
+                    specials: snapshot.data[2]['specials'],
+                    specialsIndex: 2,
+                    refresh: getSpecialsForIndex,)),
             ],
           ),
         );
       }
       );
     }
+
+  void choiceAction(String choice) {
+    if(choice == MenuItems.Settings) {
+      print('Settings');
+    }
+    else if(choice == MenuItems.SignIn) {
+      print('Sign In');
+    }
+    else if(choice == MenuItems.SignOut) {
+      print('Sign Out');
+    }
+  }
 }
 
 Color colorForTab(int tabIndex){
