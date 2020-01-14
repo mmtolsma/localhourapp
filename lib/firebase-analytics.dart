ff1aad2e88ab607ef37f8f0b68a6c35c9323e48d
mmtolsma <mtolsma19@gmail.com>
2020-01-14 17:45:56 +0200

    Added 'setUserProperties' to the onLogin event

--- lib/firebase-analytics.dart
+++ lib/firebase-analytics.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:localhour/global-data.dart';
 
 final FirebaseAnalytics analytics = FirebaseAnalytics();
 
@@ -9,6 +10,15 @@ class FireBaseAnalyticsData {
     {
       analytics.logLogin();
       print("Log in successful");
+
+      analytics.setUserProperty(name: 'Username', value: globalData.user.displayName);
+      print("Username captured: " + globalData.user.displayName);
+
+      analytics.setUserProperty(name: 'Email', value: globalData.user.email);
+      print("Email captured: " + globalData.user.email);
+
+      analytics.setUserProperty(name: 'PhotoUrl', value: globalData.user.photoUrl);
+      print("Profile picture captured: " + globalData.user.photoUrl);
     }
     else
       print("Error delivering login stats");
