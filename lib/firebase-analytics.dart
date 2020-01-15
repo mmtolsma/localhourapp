bdc21bc60769e911f8a5add2b36760cb833435c5
mmtolsma <mtolsma19@gmail.com>
2020-01-15 18:08:59 +0200

    Added tabChanged Firebase logEvent function

--- lib/firebase-analytics.dart
+++ lib/firebase-analytics.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:localhour/global-data.dart';
 
-final FirebaseAnalytics analytics = FirebaseAnalytics();
+final FirebaseAnalytics analytics = new FirebaseAnalytics();
+var fireBaseAnalyticsDataObject = new FireBaseAnalyticsData();
 
 class FireBaseAnalyticsData {
 
+  //Record login
   void onLogin(bool result) {
     if(result)
     {
@@ -16,12 +18,51 @@ class FireBaseAnalyticsData {
 
       analytics.setUserProperty(name: 'Email', value: globalData.user.email);
       print("Email captured: " + globalData.user.email);
-
-      analytics.setUserProperty(name: 'PhotoUrl', value: globalData.user.photoUrl);
-      print("Profile picture captured: " + globalData.user.photoUrl);
     }
     else
       print("Error delivering login stats");
   }
+
+  //Record sign out
+  void onSignOut(bool result) {
+    if(result) {
+      analytics.logEvent(
+          name: 'Signed_out',
+          parameters: {'User_name': globalData.user.displayName});
+    }
+    else
+      print("Could not send sign out stats");
+  }
+
+  //Record tab change on swipe and tap
+  void tabChanged(int tabIndex, int index) {
+
+    String tabTitle;
+
+    switch(index) { //This is not ideal, but I don't know how to pass in the title data dynamically
+      case 0:
+        tabTitle = "Today";
+        break;
+      case 1:
+        tabTitle = "Tomorrow";
+        break;
+      case 2:
+        tabTitle = "Next_day"; //Can't have spaces for FireBase events
+        break;
+      default:
+        tabTitle = "Error";
+    }
+
+    if(index != tabIndex) {
+      analytics.logEvent(
+          name: "$tabTitle" + "_Tab_View",
+          parameters: {'Tab_Name': tabTitle}
+      );
+    }
+  }
 }
+
+
+
+
 
