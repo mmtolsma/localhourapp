9148dd81d30417ea3641b9282a7531fad9d9aa94
mmtolsma <mtolsma19@gmail.com>
2020-01-23 14:22:58 +0200

    Fixed specialExpanded function to track collapse and expand of tapped sp

--- lib/firebase-analytics.dart
+++ lib/firebase-analytics.dart
@@ -5,26 +5,33 @@ final FirebaseAnalytics analytics = new FirebaseAnalytics();
 var fireBaseAnalyticsDataObject = new FireBaseAnalyticsData();
 
 class FireBaseAnalyticsData {
-
   //Record login
   void onLogin(bool result) {
     if(result)
     {
       analytics.logLogin();
-      analytics.setUserProperty(name: 'Username', value: globalData.user.displayName);
-      analytics.setUserProperty(name: 'Email', value: globalData.user.email);
+
+      analytics.setUserProperty(
+        name: 'Username',
+        value: globalData.user.displayName);
+
+      analytics.setUserProperty(
+        name: 'Email',
+        value: globalData.user.email);
     }
     else
       print("Error delivering login stats");
   }
 
-//  //Record sign out
+  //Record sign out
   void onSignOut(bool result) {
     if(result) {
       analytics.logEvent(
-          name: 'Signed_out',
-          parameters: {'User_name': "Still_an_issue"}); //globalData.user.displayName.toString()
-          //Doesn't work when user is signed in from before
+        name: 'Signed_out',
+        parameters: {
+          'User_name': globalData.user.displayName} //to do. Not sure how to pass the display name here.
+        ); //globalData.user.displayName.toString()
+        //Doesn't work when user is signed in from before
     }
     else
       print("Could not send sign out stats");
@@ -35,13 +42,30 @@ class FireBaseAnalyticsData {
     specials.then((data) {
       if(currentIndex != prevIndex) {
         analytics.logEvent(
-            name: "tab_changed",
-            parameters: {'tab_name': data[currentIndex]['title'],
-              'prev_tab_name': data[prevIndex]['title']}
+          name: "tab_changed",
+          parameters: {
+            'tab_name': data[currentIndex]['title'],
+            'prev_tab_name': data[prevIndex]['title'],
+          }
         );
       }
     });
   }
+
+  void specialExpanded(String venue, String specialSummary, String dayOfWeek, bool isExpanded) {
+    var tappedState = isExpanded == true ? "expanded" : "collapsed";
+
+    analytics.logEvent(
+        name: 'special_tapped',
+        parameters: {
+          'venue' : venue,
+          'special_summary' : specialSummary,
+          'day_of_week' : dayOfWeek,
+          'expanded_state' : tappedState,
+        }
+    );
+  }
+
 }
 
 
